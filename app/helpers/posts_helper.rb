e90b395328be994859f43f82a2836ea66e0de620
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-17 16:46:32 +0100

    add css for the posts and linters

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,28 +1,28 @@
 module PostsHelper
   def post_info(post)
     if user_signed_in?
-      content_tag(:div) do
-        content_tag(:p, post.user.email) +
-          content_tag(:p, post.title) +
-          content_tag(:p, post.body)
+      content_tag(:div, class: 'list-group-item test  text-center list-group-item-action list-group-item-secondary') do
+        content_tag(:p, post.user.email, class: 'd-flex text-white') +
+          content_tag(:p, post.title, class: 'h2 bg-white test8 text-uppercase') +
+          content_tag(:p, post.body, class: 'bg-white test8 h-100 text-capitalize rounded')
       end
     else
-      content_tag(:div) do
-        content_tag(:p, post.title) +
-          content_tag(:p, post.body)
+      content_tag(:div, class: 'list-group-item test  text-center list-group-item-action list-group-item-secondary') do
+        content_tag(:p, post.title, class: 'h2 bg-white test8 text-uppercase') +
+          content_tag(:p, post.body, class: 'bg-white test8 h-100 text-capitalize rounded')
       end
     end
   end
 
   def user_sessions
     if user_signed_in?
-      content_tag(:div) do
-        (button_to 'sign out', destroy_user_session_path, method: :delete) +
-          (button_to 'new post', new_post_path, method: :delete)
+      content_tag(:div, class: 'd-flex special') do
+        (button_to 'sign out', destroy_user_session_path, method: :delete, class: 'btn_all') +
+          (button_to 'new post', new_post_path, method: :delete, class: 'btn_all')
       end
     else
-      (button_to 'sign in', new_user_session_path) +
-        (button_to 'sign up', new_user_registration_path)
+      (button_to 'sign in', new_user_session_path, class: 'btn_all') +
+        (button_to 'sign up', new_user_registration_path, class: 'btn_all')
     end
   end
 end
