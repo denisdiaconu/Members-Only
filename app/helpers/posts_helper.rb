63b681902d75be994b7fa9c26ea40eafa9d32326
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-18 08:52:17 +0100

    fixed the button links

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -18,11 +18,11 @@ module PostsHelper
     if user_signed_in?
       content_tag(:div, class: 'd-flex special') do
         (button_to 'sign out', destroy_user_session_path, method: :delete, class: 'btn_all') +
-          (button_to 'new post', new_post_path, method: :delete, class: 'btn_all')
+          (link_to 'new post', new_post_path, method: :delete, class: 'btn_all')
       end
     else
       (button_to 'sign in', new_user_session_path, class: 'btn_all') +
-        (button_to 'sign up', new_user_registration_path, class: 'btn_all')
+        (link_to 'sign up', new_user_registration_path, class: 'btn_all')
     end
   end
 end
